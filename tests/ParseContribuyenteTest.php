be501577caa504cfdd0b01dcd6059d06ea9b0235
Wilsen Hernández <wilsenh95@gmail.com>
2021-07-23 09:40:00 -0400

    añadir tests condicion tributaria

--- tests/ParseContribuyenteTest.php
+++ tests/ParseContribuyenteTest.php
@@ -21,7 +21,8 @@ test('parse contribuyente natural', function () {
             ['nombre' => 'FIRMA PERSONAL 3'],
         ])
         ->actividad_economica->toEqual('INFORMACION NO DISPONIBLE')
-        ->registro_vencido->toEqual(false);
+        ->registro_vencido->toEqual(false)
+        ->condicion->toEqual('La condición de este contribuyente requiere la retención del 100% del impuesto causado, salvo que esté exento, no sujeto o demuestre ante el Agente de Retención del IVA que es un contribuyente exonerado.');
 });
 
 test('parse contribuyente juridico', function () {
@@ -38,7 +39,8 @@ test('parse contribuyente juridico', function () {
         ->nombre_comercial->toEqual('TRIENLACE, C.A')
         ->firma_personal->toHaveCount(0)
         ->actividad_economica->toEqual('VENTA AL POR MAYOR DE OTROS PRODUCTOS NO ESPECIALIZADOS')
-        ->registro_vencido->toEqual(false);
+        ->registro_vencido->toEqual(false)
+        ->condicion->toEqual('La condición de este contribuyente requiere la retención del 100% del impuesto causado, salvo que esté exento, no sujeto o demuestre ante el Agente de Retención del IVA que es un contribuyente exonerado.');
 });
 
 
@@ -56,5 +58,6 @@ test('parse contribuyente gubernamental', function () {
         ->nombre_comercial->toEqual('SERVICIO NACIONAL INTEGRADO DE ADMINISTRACION ADUANERA Y TRIBUTARIA')
         ->firma_personal->toHaveCount(0)
         ->actividad_economica->toEqual('INFORMACION NO DISPONIBLE')
-        ->registro_vencido->toEqual(false);
+        ->registro_vencido->toEqual(false)
+        ->condicion->toEqual('Contribuyente Ordinario del IVA  y Agente de Retención del IVA  La condición de este contribuyente requiere la retención del 75% del impuesto causado, salvo que incurra en los supuestos establecidos para la retención del 100%.');
 });
